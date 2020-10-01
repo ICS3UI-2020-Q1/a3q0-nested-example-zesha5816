0afe3bec2f343a2ad876f469b2d32187ebc5ab9c
zesha5816 <zesha5816@wrdsb.ca>
2020-10-01 13:14:51 +0000

    finished nested if statements

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *The example program for Nested
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -9,7 +10,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+		Scanner input = new Scanner(System.in);
+
+		//ask the user if the animal has feathers
+		System.out.println("Does the animal have feathers? (yes/no)");
+		String featherAnswer = input.nextLine();
     
-  }
+		//check to see feather status
+		if(featherAnswer.equals("yes")){
+			//get feather colour
+			System.out.println("What colour are the feathers? (red/blue)");
+			String featherColour = input.nextLine();
+			if(featherColour.equals("blue")){
+				System.out.println("You have a blue jay");
+			}else if(featherColour.equals("red")){
+				System.out.println("You have a cardinal");
+			}
+			//if the user answered no
+		}else{
+			
+		}
+	}
 }
